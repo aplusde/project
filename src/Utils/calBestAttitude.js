736add5f98616937ba00198c1e534ce354be60f7
Kittinut Pramhhan <tong.kittinut@maqe.com>
2020-01-25 21:36:47 +0700

    calculate loop and 4 model

--- src/Utils/calBestAttitude.js
+++ src/Utils/calBestAttitude.js
@@ -5,10 +5,8 @@ import createMatrix from './createMatrix';
 export const calCulateAttitude = (prod = []) => {
   const { id, attitude } = prod[prod.length - 1];
   let maxRange = 0;
-  const range = createRangeTable(prod); //calc range
-  //   {
-  //       range: [303.23,323.323]
-  //   }
+  const range = createRangeTable(prod);
+
   range.map(({ range }) => {
     return range.map(v => {
       if (v > maxRange) {
@@ -17,38 +15,270 @@ export const calCulateAttitude = (prod = []) => {
     });
   });
 
-  const allRangeOfNodes = range.map(({ range }) => range); //[303.23,323.323]
+  const allRangeOfNodes = range.map(({ range }) => range);
   const rangeArray = [];
 
   for (let i = 1; i <= 10; i++) {
-    rangeArray.push((i * maxRange) / 10); // 100 [100,200,300,400]
+    rangeArray.push((i * maxRange) / 10);
   }
+  const data = calculateBestNuggetSillRange(range, maxRange);
+  console.log(data);
 
-  let bestNugget = 0;
-  let bestSill = 0.1;
-  let min = 10;
-  let bestSum = 0;
-  let semiVarioGram;
+  const bestNugget = 0;
+  const bestSill = 0.1;
   const vairiantNodeObject = tranformSemivariance(range)(
     bestNugget,
     bestSill,
     maxRange
   );
+
+  const modelexponential = calBestAttitudeLastNode(
+    vairiantNodeObject,
+    'exponential'
+  );
+
+  const modelinear = calBestAttitudeLastNode(vairiantNodeObject, 'linear');
+  const modelspherical = calBestAttitudeLastNode(
+    vairiantNodeObject,
+    'spherical'
+  );
+  console.log({
+    vairiantNodeObject,
+    modelinear,
+    modelexponential,
+    modelspherical
+  });
+
+  return {
+    bestSum: modelexponential.sum, //node attitue 31
+    allRangeOfNodes,
+    semiVarioGram: vairiantNodeObject.map(({ exponential }) => exponential)
+  };
+};
+
+const calBestAttitudeLastNode = (vairiantNodeObject, model = 'exponential') => {
   const convertMatrix = createMatrix(vairiantNodeObject);
   let A = convertMatrix;
-  let b = vairiantNodeObject[vairiantNodeObject.length - 1].semi;
+  let b = vairiantNodeObject[vairiantNodeObject.length - 1][model];
   let w = math.multiply(math.inv(A), b);
   let sum = 0;
   for (let i = 0; i < vairiantNodeObject.length - 1; i += 1) {
     sum += vairiantNodeObject[i].attitude * w[i];
   }
-  const error = math.sum(
-    math.dotMultiply(vairiantNodeObject[vairiantNodeObject.length - 1].semi, w)
+  const errorPedictionModel = math.sum(
+    math.dotMultiply(
+      vairiantNodeObject[vairiantNodeObject.length - 1][model],
+      w
+    )
   );
+  // to do add error model 5 mel
+  return {
+    sum,
+    errorPedictionModel
+  };
+};
+
+const calculateBestNuggetSillRange = (range, maxRange) => {
+  let nuggetArray = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]; //11 //10
+  let sillArray = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]; //  10
+  let minError = {
+    exponential: 0,
+    linear: 0,
+    spherical: 0,
+    pentaspherical: 0,
+    gaussian: 0
+  };
+
+  let semiVarioGram = {
+    exponential: [],
+    linear: [],
+    spherical: [],
+    pentaspherical: [],
+    gaussian: []
+  };
+
+  let mockBestNugget = {
+    exponential: 0,
+    linear: 0,
+    spherical: 0,
+    pentaspherical: 0,
+    gaussian: 0
+  };
+
+  let mockBestSil = {
+    exponential: 0,
+    linear: 0,
+    spherical: 0,
+    pentaspherical: 0,
+    gaussian: 0
+  };
+
+  let mockBestRange = {
+    exponential: 0,
+    linear: 0,
+    spherical: 0,
+    pentaspherical: 0,
+    gaussian: 0
+  };
+
+  let bestSum = {
+    exponential: 0,
+    linear: 0,
+    spherical: 0,
+    pentaspherical: 0,
+    gaussian: 0
+  };
+
+  for (let i = 0; i < nuggetArray.length; i++) {
+    for (let j = 0; j < sillArray.length; j++) {
+      const vairiantNodeObject = tranformSemivariance(range)(
+        +nuggetArray[i],
+        +sillArray[j],
+        maxRange
+      );
+
+      const modelexponential = calBestAttitudeLastNode(
+        vairiantNodeObject,
+        'exponential'
+      );
+
+      const modelinear = calBestAttitudeLastNode(vairiantNodeObject, 'linear');
+      const modelspherical = calBestAttitudeLastNode(
+        vairiantNodeObject,
+        'spherical'
+      );
+      const modelpentaspherical = calBestAttitudeLastNode(
+        vairiantNodeObject,
+        'pentaspherical'
+      );
+      const modelgussian = calBestAttitudeLastNode(
+        vairiantNodeObject,
+        'gaussian'
+      );
+      console.log(modelgussian.errorPedictionModel, modelgussian.sum);
+
+      if (minError['gaussian'] === 0) {
+        minError['gaussian'] = modelgussian.errorPedictionModel;
+        mockBestNugget['gaussian'] = nuggetArray[i];
+        mockBestSil['gaussian'] = sillArray[j];
+        mockBestRange['gaussian'] = maxRange;
+        bestSum['gaussian'] = modelgussian.sum;
+        semiVarioGram['gaussian'] = vairiantNodeObject.map(
+          ({ gaussian }) => gaussian
+        );
+      }
+      if (modelgussian.errorPedictionModel < minError['gaussian']) {
+        minError['gaussian'] = modelgussian.errorPedictionModel;
+        mockBestNugget['gaussian'] = nuggetArray[i];
+        mockBestSil['gaussian'] = sillArray[j];
+        mockBestRange['gaussian'] = maxRange;
+        bestSum['gaussian'] = modelgussian.sum;
+        semiVarioGram['gaussian'] = vairiantNodeObject.map(
+          ({ gaussian }) => gaussian
+        );
+      }
+      //
+
+      if (minError['pentaspherical'] === 0) {
+        minError['pentaspherical'] = modelpentaspherical.errorPedictionModel;
+        mockBestNugget['pentaspherical'] = nuggetArray[i];
+        mockBestSil['pentaspherical'] = sillArray[j];
+        mockBestRange['pentaspherical'] = maxRange;
+        bestSum['pentaspherical'] = modelpentaspherical.sum;
+        semiVarioGram['pentaspherical'] = vairiantNodeObject.map(
+          ({ pentaspherical }) => pentaspherical
+        );
+      }
+      if (
+        modelpentaspherical.errorPedictionModel < minError['pentaspherical']
+      ) {
+        minError['pentaspherical'] = modelpentaspherical.errorPedictionModel;
+        mockBestNugget['pentaspherical'] = nuggetArray[i];
+        mockBestSil['pentaspherical'] = sillArray[j];
+        mockBestRange['pentaspherical'] = maxRange;
+        bestSum['pentaspherical'] = modelpentaspherical.sum;
+        semiVarioGram['pentaspherical'] = vairiantNodeObject.map(
+          ({ pentaspherical }) => pentaspherical
+        );
+      }
+
+      /*linear*/
+      if (minError['linear'] === 0) {
+        minError['linear'] = modelinear.errorPedictionModel;
+        mockBestNugget['linear'] = nuggetArray[i];
+        mockBestSil['linear'] = sillArray[j];
+        mockBestRange['linear'] = maxRange;
+        bestSum['linear'] = modelinear.sum;
+        semiVarioGram['linear'] = vairiantNodeObject.map(
+          ({ linear }) => linear
+        );
+      }
+      if (modelinear.errorPedictionModel < minError['linear']) {
+        minError['linear'] = modelinear.errorPedictionModel;
+        mockBestNugget['linear'] = nuggetArray[i];
+        mockBestSil['linear'] = sillArray[j];
+        mockBestRange['linear'] = maxRange;
+        bestSum['linear'] = modelinear.sum;
+        semiVarioGram['linear'] = vairiantNodeObject.map(
+          ({ linear }) => linear
+        );
+      }
+      /*linear*/
+
+      /* spherical */
+      if (minError['spherical'] === 0) {
+        minError['spherical'] = modelspherical.errorPedictionModel;
+        mockBestNugget['spherical'] = nuggetArray[i];
+        mockBestSil['spherical'] = sillArray[j];
+        mockBestRange['spherical'] = maxRange;
+        bestSum['spherical'] = modelspherical.sum;
+        semiVarioGram['spherical'] = vairiantNodeObject.map(
+          ({ spherical }) => spherical
+        );
+      }
+      if (modelspherical.errorPedictionModel < minError['spherical']) {
+        minError['spherical'] = modelspherical.errorPedictionModel;
+        mockBestNugget['spherical'] = nuggetArray[i];
+        mockBestSil['spherical'] = sillArray[j];
+        mockBestRange['spherical'] = maxRange;
+        bestSum['spherical'] = modelspherical.sum;
+        semiVarioGram['spherical'] = vairiantNodeObject.map(
+          ({ spherical }) => spherical
+        );
+      }
+      /* spherical */
+
+      /*exponential*/
+      if (minError['exponential'] === 0) {
+        minError['exponential'] = modelexponential.errorPedictionModel;
+        mockBestNugget['exponential'] = nuggetArray[i];
+        mockBestSil['exponential'] = sillArray[j];
+        mockBestRange['exponential'] = maxRange;
+        bestSum['exponential'] = modelexponential.sum;
+        semiVarioGram['exponential'] = vairiantNodeObject.map(
+          ({ exponential }) => exponential
+        );
+      }
+      if (modelexponential.errorPedictionModel < minError['exponential']) {
+        minError['exponential'] = modelexponential.errorPedictionModel;
+        mockBestNugget['exponential'] = nuggetArray[i];
+        mockBestSil['exponential'] = sillArray[j];
+        mockBestRange['exponential'] = maxRange;
+        bestSum['exponential'] = modelexponential.sum;
+        semiVarioGram['exponential'] = vairiantNodeObject.map(
+          ({ exponential }) => exponential
+        );
+      }
+      /*exponential*/
+    }
+  }
 
   return {
-    bestSum: sum, //node attitue 31
-    allRangeOfNodes,
-    semiVarioGram: vairiantNodeObject.map(({ semi }) => semi)
+    mockBestRange,
+    mockBestNugget,
+    mockBestSil,
+    bestSum,
+    semiVarioGram,
+    minError
   };
 };
--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,217 +1,232 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Plot from 'react-plotly.js';
-import memoize from 'fast-memoize'
-import { calCulateAttitude } from '../Utils/calBestAttitude'
-import Loader from 'react-loader-spinner'
-import { connect } from 'react-redux'
-import { getAllNode } from '../actions'
+import memoize from 'fast-memoize';
+import { calCulateAttitude } from '../Utils/calBestAttitude';
+import Loader from 'react-loader-spinner';
+import { connect } from 'react-redux';
+import { getAllNode } from '../actions';
 import * as XLSX from 'xlsx';
 import getXYZ from '../Utils/getXYZ';
 
-const memoizeCalCulateAttitude = memoize(calCulateAttitude)
+const memoizeCalCulateAttitude = memoize(calCulateAttitude);
 class Form extends Component {
-    state = {
-        nodes: [
-            { id: 1, latitude: '', longtitude: '', attitude: '' },
-        ],
-        x: [],
-        y: [],
-        z: [],
-        loading: false,
-    }
-    addNode = () => {
-        const { nodes } = this.state
-        const id = nodes.length + 1
-        this.setState({
-            nodes: [
-                ...nodes,
-                {
-                    id: id,
-                    latitude: '',
-                    longtitude: '',
-                    attitude: ''
-                }
-            ]
-        })
-    }
-    onChangeFile = e => {
-        const file = e.target.files[0];
-        var name = file.name;
-        const reader = new FileReader();
-        let dataRetrive
-        reader.onload = (evt) => {
-            /* Parse data */
-            const bstr = evt.target.result;
-            const wb = XLSX.read(bstr, { type: 'binary' });
-            /* Get first worksheet */
-            const wsname = wb.SheetNames[0];
-            const ws = wb.Sheets[wsname];
-            /* Convert array of arrays */
-            const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
-            /* Update state */
-            data.shift()
-            const transformDataNode = data.reduce((array, next, index) => {
-                return [
-                    ...array,
-                    {
-                        id: index + 1,
-                        latitude: next[0],
-                        longtitude: next[1],
-                        attitude: next[2]
-                    }
-                ]
-            }, [])
-            this.setState({
-                nodes: transformDataNode
-            })
-        };
-        reader.readAsBinaryString(file);
-    }
-    onChangeNode = (e) => {
-        const { nodes } = this.state
-        const { id, name, value } = e.target
-        const temp = nodes
-        temp[id - 1][name] = value
-        this.setState({
-            nodes: temp
-        })
-    }
-    deleteNodes = (e) => {
-        const { nodes } = this.state
-        const { id } = e.target
-        const nodeIdTarget = parseInt(id)
-        const updateDeleteNode = nodes.filter(({ id }) => id !== nodeIdTarget)
-        this.setState({
-            nodes: updateDeleteNode
-        })
-    }
-    onSubmit = () => {
-      
-        const { nodes,loading } = this.state
-        this.setState({
-            loading: !loading
-        })
-        setTimeout(()=>{
-            const { bestSum, allRangeOfNodes, semiVarioGram } = memoizeCalCulateAttitude(nodes)
-            let scatterGraph = new Array()
-            for (let i = 0; i < allRangeOfNodes.length - 1; i++) {
-                allRangeOfNodes[i].pop()
-                semiVarioGram[i].pop()
-                const range = allRangeOfNodes[i]
-                const semi = semiVarioGram[i]
-                scatterGraph.push({
-                    x: range,
-                    y: semi,
-                    mode: 'markers',
-                    name: `Node ${i + 1}`,
-                    type: 'scatter'
-                })
-            }
-            let newNodesWithLastAttitude = nodes
-            newNodesWithLastAttitude[newNodesWithLastAttitude.length - 1].attitude = bestSum
-            const x = getXYZ(newNodesWithLastAttitude, 'latitude')
-            const y = getXYZ(newNodesWithLastAttitude, 'longtitude')
-            const z = getXYZ(newNodesWithLastAttitude, 'attitude')
-            this.setState({
-                nodes: newNodesWithLastAttitude,
-                x: x,
-                y: y,
-                z: z,
-                scatterGraph,
-                loading:false
-            })
-    
-        },500)
-        
-    }
-    render() {
-        const { nodes, x, y, z,loading } = this.state;
-        return (
-            <div className="container-graph">
-                {
-                loading&&
-                <div className="modal">
-                    <Loader 
-                        type="Puff"
-                        color="#00BFFF"
-                        height="100"	
-                        width="100"
-                    />
-                </div> }  
-                <div style={{ margin: '15px' }}>
-                    <h1>FORM NODE  LIST</h1>
-                    <div className="input-node-title">
-                        <p className="node-p-id">ID</p>
-                        <p className="node-unit">Latitude</p>
-                        <p className="node-unit">Longtitude</p>
-                        <p className="node-unit">Attitude</p>
-                    </div>
-                    {
-                        nodes.map(({ id, latitude, longtitude, attitude }) => (
-                            <div key={id + latitude.toString()} className="input-node">
-                                <div className="id-node">
-                                    <p>{id}</p>
-                                </div>
-                                <div>
-                                    <input onChange={this.onChangeNode} id={id} name="latitude" value={latitude||''} ></input>
-                                </div>
-                                <div>
-                                    <input onChange={this.onChangeNode} id={id} name="longtitude" value={longtitude||''} ></input>
-                                </div>
-                                <div>
-                                    <input onChange={this.onChangeNode} id={id} name="attitude" value={attitude||''} ></input>
-                                </div>
-                                <div>
-                                    <button id={id} onClick={this.deleteNodes}>Delete</button>
-                                </div>
-
-
-                            </div>
-                        ))
-                    }
-
-                    <input onChange={this.onChangeFile} type="file"></input>
-                    <button onClick={this.addNode}>ADD  NODE</button>
-                    <button onClick={this.onSubmit}>Submit</button>
-                </div>
-                <div className="graph">
-                    {
-                        x.length > 0 ? <Plot
-                            data={[
-                                {
-                                    x: x,
-                                    y: y,
-                                    z: z,
-                                    type: 'mesh3d',
-                                    // intensity: [0, 0.33, 0.66, 10000000],
-                                    // colorscale: [
-                                    //     [0, 'rgb(255, 0, 0)'],
-                                    //     [0.5, 'rgb(0, 255, 0)'],
-                                    //     [10000000, 'rgb(0, 0, 255)']
-                                    // ]
-                                },
-                            ]}
-                            layout={{ width: 900, height: 600, title: '3D Surface Plots' }}
-                        /> : null
-                    }
-                    {
-                        this.state.scatterGraph && <Plot
-                            data={this.state.scatterGraph}
-                            layout={{
-                                width: 900, height: 600, title: 'Semivariogram Analysis',
-                                xaxis: {
-                                    title: 'Distance'
-                                },
-                                yaxis: {
-                                    title: 'Semivariogram'
-                                }
-                            }}
-                        />
-                    }
-                </div>
+  state = {
+    nodes: [{ id: 1, latitude: '', longtitude: '', attitude: '' }],
+    x: [],
+    y: [],
+    z: [],
+    loading: false
+  };
+  addNode = () => {
+    const { nodes } = this.state;
+    const id = nodes.length + 1;
+    this.setState({
+      nodes: [
+        ...nodes,
+        {
+          id: id,
+          latitude: '',
+          longtitude: '',
+          attitude: ''
+        }
+      ]
+    });
+  };
+  onChangeFile = e => {
+    const file = e.target.files[0];
+    var name = file.name;
+    const reader = new FileReader();
+    let dataRetrive;
+    reader.onload = evt => {
+      /* Parse data */
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: 'binary' });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+      /* Update state */
+      data.shift();
+      const transformDataNode = data.reduce((array, next, index) => {
+        return [
+          ...array,
+          {
+            id: index + 1,
+            latitude: next[0],
+            longtitude: next[1],
+            attitude: next[2]
+          }
+        ];
+      }, []);
+      this.setState({
+        nodes: transformDataNode
+      });
+    };
+    reader.readAsBinaryString(file);
+  };
+  onChangeNode = e => {
+    const { nodes } = this.state;
+    const { id, name, value } = e.target;
+    const temp = nodes;
+    temp[id - 1][name] = value;
+    this.setState({
+      nodes: temp
+    });
+  };
+  deleteNodes = e => {
+    const { nodes } = this.state;
+    const { id } = e.target;
+    const nodeIdTarget = parseInt(id);
+    const updateDeleteNode = nodes.filter(({ id }) => id !== nodeIdTarget);
+    this.setState({
+      nodes: updateDeleteNode
+    });
+  };
+  onSubmit = () => {
+    const { nodes, loading } = this.state;
+    this.setState({
+      loading: !loading
+    });
+    setTimeout(() => {
+      const {
+        bestSum,
+        allRangeOfNodes,
+        semiVarioGram
+      } = memoizeCalCulateAttitude(nodes);
+      let scatterGraph = new Array();
+      for (let i = 0; i < allRangeOfNodes.length - 1; i++) {
+        allRangeOfNodes[i].pop();
+        semiVarioGram[i].pop();
+        const range = allRangeOfNodes[i];
+        const semi = semiVarioGram[i];
+        scatterGraph.push({
+          x: range,
+          y: semi,
+          mode: 'markers',
+          name: `Node ${i + 1}`,
+          type: 'scatter'
+        });
+      }
+      let newNodesWithLastAttitude = nodes;
+      newNodesWithLastAttitude[
+        newNodesWithLastAttitude.length - 1
+      ].attitude = bestSum;
+      const x = getXYZ(newNodesWithLastAttitude, 'latitude');
+      const y = getXYZ(newNodesWithLastAttitude, 'longtitude');
+      const z = getXYZ(newNodesWithLastAttitude, 'attitude');
+      this.setState({
+        nodes: newNodesWithLastAttitude,
+        x: x,
+        y: y,
+        z: z,
+        scatterGraph,
+        loading: false
+      });
+    }, 500);
+  };
+  render() {
+    const { nodes, x, y, z, loading } = this.state;
+    return (
+      <div className="container-graph">
+        {loading && (
+          <div className="modal">
+            <Loader type="Puff" color="#00BFFF" height="100" width="100" />
+          </div>
+        )}
+        <div style={{ margin: '15px' }}>
+          <h1>FORM NODE LIST</h1>
+          <div className="input-node-title">
+            <p className="node-p-id">ID</p>
+            <p className="node-unit">Latitude</p>
+            <p className="node-unit">Longtitude</p>
+            <p className="node-unit">Attitude</p>
+          </div>
+          <p>nugget</p>
+          <input type="text"></input>
+          <p>sill</p>
+          <input type="text"></input>
+          {nodes.map(({ id, latitude, longtitude, attitude }) => (
+            <div key={id + latitude.toString()} className="input-node">
+              <div className="id-node">
+                <p>{id}</p>
+              </div>
+              <div>
+                <input
+                  onChange={this.onChangeNode}
+                  id={id}
+                  name="latitude"
+                  value={latitude || ''}
+                ></input>
+              </div>
+              <div>
+                <input
+                  onChange={this.onChangeNode}
+                  id={id}
+                  name="longtitude"
+                  value={longtitude || ''}
+                ></input>
+              </div>
+              <div>
+                <input
+                  onChange={this.onChangeNode}
+                  id={id}
+                  name="attitude"
+                  value={attitude || ''}
+                ></input>
+              </div>
+              <div>
+                <button id={id} onClick={this.deleteNodes}>
+                  Delete
+                </button>
+              </div>
             </div>
-        )
-    }
+          ))}
+
+          <input onChange={this.onChangeFile} type="file"></input>
+          <button onClick={this.addNode}>ADD NODE</button>
+          <button onClick={this.onSubmit}>Submit</button>
+        </div>
+        <div className="graph">
+          {x.length > 0 ? (
+            <Plot
+              data={[
+                {
+                  x: x,
+                  y: y,
+                  z: z,
+                  type: 'mesh3d'
+                  // intensity: [0, 0.33, 0.66, 10000000],
+                  // colorscale: [
+                  //     [0, 'rgb(255, 0, 0)'],
+                  //     [0.5, 'rgb(0, 255, 0)'],
+                  //     [10000000, 'rgb(0, 0, 255)']
+                  // ]
+                }
+              ]}
+              layout={{ width: 900, height: 600, title: '3D Surface Plots' }}
+            />
+          ) : null}
+          {this.state.scatterGraph && (
+            <Plot
+              data={this.state.scatterGraph}
+              layout={{
+                width: 900,
+                height: 600,
+                title: 'Semivariogram Analysis',
+                xaxis: {
+                  title: 'Distance'
+                },
+                yaxis: {
+                  title: 'Semivariogram'
+                }
+              }}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Form
+export default Form;
